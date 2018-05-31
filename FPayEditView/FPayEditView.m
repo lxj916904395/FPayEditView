830bfcc94b1386eac9795367f91e4d21bde7d825
lxj916904395 <916904395@qq.com>
2018-05-31 11:29:08 +0800

    修改

--- FPayEditView/FPayEditView.m
+++ FPayEditView/FPayEditView.m
@@ -253,7 +253,7 @@
 - (instancetype)initWithFrame:(CGRect)frame config:(FPayInputConfig *)config{
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = config.keyboardBackGroundColor; colorWithAlphaComponent:.3f];
+        self.backgroundColor = config.keyboardBackGroundColor;
         _viewWidth = self.frame.size.width;
         _viewHeight = self.frame.size.height;
         _config = config;
